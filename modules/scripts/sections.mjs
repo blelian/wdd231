0cf354c5e4d0e7162583e032e6dd4804cc17983a
blelian <bullsurus@gmail.com>
2025-05-21 14:22:02 +0200

    Added a modules folder

---
+++ modules/scripts/sections.mjs
@@ -0,0 +1,13 @@
+export function setSectionSelection() {
+  const sectionSelect = document.querySelector("#sectionNumber");
+  byuiCourse.sections.forEach((section) => {
+    const option = document.createElement("option");
+    option.value = section.sectionNumber;
+    option.textContent = `${section.sectionNumber}`;
+    sectionSelect.appendChild(option);
+  });
+}
+export function populateSections(sections) { 
+     byuiCourse.sections = sections;
+     setSectionSelection();
+ };
